b67161d070295e752d47cddba41be465e2fe66f2
Andrew S <andrewsales2004@gmail.com>
2022-09-14 13:36:56 -0500

    finished license code in generateMarkdown file and updated file structure to include 'dist' folder

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -8,15 +8,42 @@ function renderLicenseBadge(license) {
   } else {
   return ' ';
   }
-}
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'no license') {
+    return `
+    [${license}](https://choosealicense.com/license/${license})
+    `;
+  } else {
+    return ' ';
+  }
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'no license') {
+    return `
+    ## [License](#table-of-contents)
+    The following license covers this application:
+    ${renderLicenseLink(license)}
+    `;
+  }
+};
+
+// this function returns license in table of contents if license is present
+function LicenseInTOC(license) {
+  if (license !== 'no license') {
+    return `
+    * [License](#license)
+    `;
+  } else {
+    return ' ';
+  }
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
